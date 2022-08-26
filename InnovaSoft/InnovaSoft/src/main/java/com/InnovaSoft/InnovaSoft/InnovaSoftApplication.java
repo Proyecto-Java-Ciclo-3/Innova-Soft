df1a744472349de5a6c14aca38799dba64437152
edwinri0s <edwinrios490@gmail.com>
2022-08-25 22:55:14 -0500

    Se crea el archivo Maven

---
+++ InnovaSoft/InnovaSoft/src/main/java/com/InnovaSoft/InnovaSoft/InnovaSoftApplication.java
@@ -0,0 +1,13 @@
+package com.InnovaSoft.InnovaSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaSoftApplication.class, args);
+	}
+
+}
---
+++ InnovaSoft/InnovaSoft/src/test/java/com/InnovaSoft/InnovaSoft/InnovaSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InnovaSoft.InnovaSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
