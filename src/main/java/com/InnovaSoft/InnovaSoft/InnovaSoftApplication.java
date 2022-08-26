d628fda73445f75d5abc9fe5d351284a0bca7526
edwinri0s <edwinrios490@gmail.com>
2022-08-26 00:24:56 -0500

    Se configura el Maven.

--- src/main/java/com/InnovaSoft/InnovaSoft/InnovaSoftApplication.java
+++ src/main/java/com/InnovaSoft/InnovaSoft/InnovaSoftApplication.java
@@ -2,12 +2,24 @@ package com.InnovaSoft.InnovaSoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class InnovaSoftApplication {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola ciclo 3, saldremos vivos de esto";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnovaSoftApplication.class, args);
 	}
+
+
 
 }
