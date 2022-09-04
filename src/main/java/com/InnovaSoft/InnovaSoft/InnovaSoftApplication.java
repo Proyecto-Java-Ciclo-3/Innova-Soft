07b2ed9020a0bd7c49e14f6a36114df8251eb5a1
edwinri0s <edwinrios490@gmail.com>
2022-09-03 22:29:46 -0500

    Se crean los servicios y controladores de empresa

--- src/main/java/com/InnovaSoft/InnovaSoft/InnovaSoftApplication.java
+++ src/main/java/com/InnovaSoft/InnovaSoft/InnovaSoftApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+//@RestController
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class InnovaSoftApplication {
 
